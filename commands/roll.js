f31297050a650f9a063024a829935de4649d9d63
Tyler Proffitt <tproffitt95@gmail.com>
2020-08-22 20:28:21 -0700

    added roll command input checks and individual roll results on output

--- commands/roll.js
+++ commands/roll.js
@@ -7,17 +7,31 @@ module.exports = {
         // Split into an array of before and after the d
         const values = command.split("d");
 
-        // TODO: validate input
+        // Convert strings into numbers
+        var number_of_dice = +values[0];
+        var die_size = +values[1];
 
-        // Convert the strings into base-10 integers
-        const number_of_dice = parseInt(values[0], 10);
-        const die_size = parseInt(values[1], 10);
-
-        var result = 0;
-        for (var i = 0; i < number_of_dice; i++){
-            result += Math.floor(Math.random() * die_size) + 1;
+        // Verify that the numbers aren't NaN
+        if (isNaN(number_of_dice) || isNaN(die_size)){
+            message.channel.send("Do not waste my time with your foolish commands");
+            return;
         }
 
-        message.channel.send("Rolling " + values[0] + " d" + values[1] + "s" + '\n' + result);
+        // Remove any decimals if a float was passed
+        number_of_dice = Math.floor(number_of_dice);
+        die_size = Math.floor(die_size);
+
+        // Perform the rolls
+        var results = new Array();
+        var roll = 0;
+        var total = 0;
+        for (var i = 0; i < number_of_dice; i++){
+            roll = Math.floor(Math.random() * die_size) + 1;
+            results.push(roll);
+            total += roll;
+        }
+
+        // Send the individual results and the total, bolding the total
+        message.channel.send(results + " = **" + total + "**");
     }
 }
