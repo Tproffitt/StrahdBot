3fe89f060c88737d383a2c992f21796192a04eff
Tyler Proffitt <tproffitt95@gmail.com>
2020-08-26 18:58:14 -0700

    roll now handles 0 and negative values

--- commands/roll.js
+++ commands/roll.js
@@ -6,13 +6,18 @@ module.exports = {
         const command = args.shift();
         // Split into an array of before and after the d
         const values = command.split("d");
+message.channel.send(values);
+        // Convert strings into numbers (absolute value if a negative is passed)
+        var number_of_dice = Math.abs(+values[0]);
+        var die_size = Math.abs(+values[1]);
 
-        // Convert strings into numbers
-        var number_of_dice = +values[0];
-        var die_size = +values[1];
+        // If no number of dice is given, assume it is a single roll
+        if (number_of_dice == 0) {
+            number_of_dice = 1;
+        }
 
-        // Verify that the numbers aren't NaN
-        if (isNaN(number_of_dice) || isNaN(die_size)){
+        // Verify that the results aren't Not a Number
+        if (isNaN(number_of_dice) || isNaN(die_size) || die_size == 0){
             message.channel.send("Do not waste my time with your foolish commands");
             return;
         }
