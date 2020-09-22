1734a5493d6a225eab707e7c09069b89b8e8e041
Tyler Proffitt <tproffitt95@gmail.com>
2020-09-22 11:59:50 -0700

    made rolling into its own function 'makeRoll' and made individual numbers a valid input

--- commands/roll.js
+++ commands/roll.js
@@ -2,6 +2,13 @@ module.exports = {
     name: 'roll',
     description: "rolls dice",
     execute(message, args){
+        var total = 0;              // total: holds the running total of the rolls
+        var results = new String(); // results: holds the string built from the roll results
+
+        var first = true;           // first: flag inticating if this is the first argument
+        var roll_needed = true;     // roll_needed: flag inticating if a call to makeRoll is required
+        var bad = false;            // bad: flag inticating if a bad argument has been passed
+
         // Take the first argument which should be of the form #d#
         const command = args.shift();
         // Split into an array of before and after the d
@@ -17,28 +24,47 @@ module.exports = {
         }
 
         // Verify that the results aren't Not a Number
-        if (isNaN(number_of_dice) || isNaN(die_size)){
-            throw "The concept of numbers seems beyond you...I am not surprised";
-            return;
+        if (isNaN(number_of_dice) && isNaN(die_size)){
+            if (first){
+                throw "The concept of numbers seems beyond you...I am not surprised";
+                return;
+            } else {
+                bad = true;
+            }
+        } else if (!isNaN(number_of_dice) && isNaN(die_size)){      // If only a number is passed, return that number: !r 2 => 2
+            total += number_of_dice;
+            results += number_of_dice;
+            roll_needed = false;
         }
 
-        // Remove any decimals if a float was passed
-        number_of_dice = Math.floor(number_of_dice);
-        die_size = Math.floor(die_size);
+        if (roll_needed && !bad){
+            // Remove any decimals if a float was passed
+            number_of_dice = Math.floor(number_of_dice);
+            die_size = Math.floor(die_size);
 
-        // Perform the rolls
-        var results = new Array();
-        var minus = 0;
-        var roll = 0;
-        var total = 0;
-        for (var i = 0; i < number_of_dice; i++){
-            minus = Math.floor(Math.random() * die_size);   // Gets a value between 0 and (die_size - 1) (inclusive)
-            roll = die_size - minus;                        // die_size - minus to be inclusive of die_size of 0
-            results.push(roll);
-            total += roll;
+            // Perform the rolls
+            output = this.makeRoll(number_of_dice, die_size);
+
+            results += '[' + output + ']';
+            while (roll = output.shift()){
+                total += roll;
+            }
         }
 
         // Send the individual results and the total, bolding the total
-        message.channel.send(results + " = **" + total + "**");
+        message.channel.send(results + "\n**" + total + "**");
+    },
+    makeRoll(number_of_dice, die_size){
+        var rolls = new Array();
+        var minus = 0;
+        var roll = 0;
+
+        for (var i = 0; i < number_of_dice; i++){
+            minus = Math.floor(Math.random() * die_size);   // Gets a value between 0 and (die_size - 1) (inclusive)
+            roll = die_size - minus;                        // die_size - minus to be inclusive of die_size of 0
+            rolls.push(roll);
+        }
+
+        return rolls;
     }
 }
